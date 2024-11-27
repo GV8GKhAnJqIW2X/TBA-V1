c9617077d1b9d693670df1329d58ae4867260720
mac2UZaM583 <163586801+mac2UZaM583@users.noreply.github.com>
2024-11-27 19:54:29 +0300

    Delete onion directory

--- onion/l1/s_logging.py
+++
@@ -1,9 +0,0 @@
-from loguru import logger
-
-logger.add(
-    "logs/{time:DD-MM-YYYY}.log", 
-    format="{time} {level} {message}", 
-    level="INFO",
-    rotation="00:00", 
-    serialize=False,
-)
