6a69938db8abdf4a1817a85bf8e449a06e2a913b
name lastname <you@example.com>
2024-12-09 03:21:49 +0700

    sss

--- main.py
+++ main.py
@@ -10,66 +10,66 @@ from datetime import datetime
 async def main():
     # init
     symbol = "SUIUSDT"
-    src = g_src(**(await g_klines_split(await g_klines(
-        symbol=symbol, 
-        qty=(
-            settings["SIGNAL_GENERATION"]["max_window_features"] * 2
-            + settings["SIGNAL_GENERATION"]["ML"]["klines_train"] 
-            + settings["BACKTEST"]["klines_test"]
-            + 1
-        )
-    ))))
-    close_backtest = src["close"][-settings["BACKTEST"]["klines_test"]:]
     signals = np.empty(settings["BACKTEST"]["klines_test"])
-    signals_raw_array = np.full(settings["BACKTEST"]["klines_test"], np.nan)
-    x_features_series_array = np.full(settings["BACKTEST"]["klines_test"], {})
-    filters_values_array = np.full(settings["BACKTEST"]["klines_test"], {})
-    balances_array = np.full(settings["BACKTEST"]["klines_test"], np.nan)
-    signal_return_value = None
     datetime_now = datetime.now().strftime('%Y.%m.%d_%H.%M.%S')
 
+    # src = g_src(**(await g_klines_split(await g_klines(
+    #     symbol=symbol, 
+    #     qty=(
+    #         settings["SIGNAL_GENERATION"]["max_window_features"] * 2
+    #         + settings["SIGNAL_GENERATION"]["ML"]["klines_train"] 
+    #         + settings["BACKTEST"]["klines_test"]
+    #         + 1
+    #     )
+    # ))))
+    # close_backtest = src["close"][-settings["BACKTEST"]["klines_test"]:]
+    # signals_raw_array = np.full(settings["BACKTEST"]["klines_test"], np.nan)
+    # x_features_series_array = np.full(settings["BACKTEST"]["klines_test"], {})
+    # filters_values_array = np.full(settings["BACKTEST"]["klines_test"], {})
+    # balances_array = np.full(settings["BACKTEST"]["klines_test"], np.nan)
+    # signal_return_value = None
+
     # ready_made_data
-    # with open("backtest_values/SUIUSDT_kt50000_2024.12.08_23.18.25.pickle", "rb") as f:
-    #     ready_made_data = pickle.load(f)
-    
-    # close_backtest = ready_made_data["close"]
-    # signals_raw_array = ready_made_data["signals_raw_array"]
-    # filters_values_array = ready_made_data["filters_values_array"]
+    with open("backtest_values/SUIUSDT_kt50000_2024.12.09_01.27.55.pickle", "rb") as f:
+        ready_made_data = pickle.load(f)
+    close_backtest = ready_made_data["close"]
+    signals_raw_array = ready_made_data["signals_raw_array"]
+    filters_values_array = ready_made_data["filters_values_array"]
 
     # main
     for i in range(settings["BACKTEST"]["klines_test"]):
-        signal, additional_return, signal_return_value = g_signal_A_distance_lorentzian_A_ANN_A_iter(
-            **g_iloc(src, slice(
-                i, 
-                (
-                    i 
-                    + settings["SIGNAL_GENERATION"]["max_window_features"] * 2 
-                    + settings["SIGNAL_GENERATION"]["ML"]["klines_train"] 
-                    + 1
-                )
-            )),
-            initialized_return_value=signal_return_value,
-            additional_return_x_features_series=True,
-            additional_return_signal_raw=True,
-            additional_return_filters_values=True,
-        )
-        (
-            x_features_series, 
-            signal_raw, 
-            filters_values,
-        ) = additional_return
-        signals[i] = signal
-        x_features_series_array[i] = x_features_series
-        signals_raw_array[i] = signal_raw
-        filters_values_array[i] = filters_values
-
-        # # ready_made_data
-        # signals[i] = g_signal_A_ready_made_data(
-        #     signals_raw_array[i],
-        #     close_backtest[i],
-        #     filters_values_array[i],
-        #     settings["SIGNAL_GENERATION"]["filters_used"],
+        # signal, additional_return, signal_return_value = g_signal_A_distance_lorentzian_A_ANN_A_iter(
+        #     **g_iloc(src, slice(
+        #         i, 
+        #         (
+        #             i 
+        #             + settings["SIGNAL_GENERATION"]["max_window_features"] * 2 
+        #             + settings["SIGNAL_GENERATION"]["ML"]["klines_train"] 
+        #             + 1
+        #         )
+        #     )),
+        #     initialized_return_value=signal_return_value,
+        #     additional_return_x_features_series=True,
+        #     additional_return_signal_raw=True,
+        #     additional_return_filters_values=True,
         # )
+        # (
+        #     x_features_series, 
+        #     signal_raw, 
+        #     filters_values,
+        # ) = additional_return
+        # signals[i] = signal
+        # x_features_series_array[i] = x_features_series
+        # signals_raw_array[i] = signal_raw
+        # filters_values_array[i] = filters_values
+
+        # ready_made_data
+        signals[i] = g_signal_A_ready_made_data(
+            signals_raw_array[i],
+            close_backtest[i],
+            filters_values_array[i],
+            settings["SIGNAL_GENERATION"]["filters_used"],
+        )
         
         print(f"\r{i + 1}/{settings["BACKTEST"]["klines_test"]} klines tested", end="")
     
@@ -98,17 +98,17 @@ async def main():
     fig.write_html(f"plotly_html/{symbol}_kt{settings["BACKTEST"]["klines_test"]}_{datetime_now}.html")
     fig.show()
 
-    with open(f"backtest_values/{symbol}_kt{settings["BACKTEST"]["klines_test"]}_{datetime_now}.pickle", "wb") as f:
-        pickle.dump(
-            {
-                "signals":signals,
-                "close": close_backtest,
-                "x_features_series_array": x_features_series_array,
-                "signals_raw_array": signals_raw_array,
-                "filters_values_array": filters_values_array,
-            },
-            f,
-        )
+    # with open(f"backtest_values/{symbol}_kt{settings["BACKTEST"]["klines_test"]}_{datetime_now}.pickle", "wb") as f:
+    #     pickle.dump(
+    #         {
+    #             "signals":signals,
+    #             "close": close_backtest,
+    #             "x_features_series_array": x_features_series_array,
+    #             "signals_raw_array": signals_raw_array,
+    #             "filters_values_array": filters_values_array,
+    #         },
+    #         f,
+    #     )
     
 import asyncio
 asyncio.run(main())
